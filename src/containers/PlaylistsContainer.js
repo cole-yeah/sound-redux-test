a2c77193a2d8b5f5a605737d976ba71b7f56ef9e
chenlc <243461254@qq.com>
2017-09-19 14:39:22 +0800

    更新样式文件

--- src/containers/PlaylistsContainer.js
+++ src/containers/PlaylistsContainer.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react'
 
 class PlaylistsContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            hasError: false
+        }
+    }
+    componentDidCatch(error, info) {
+        this.setState({hasError: true})
+        console.log(error, info)
+    }
     render() {
+        if(this.state.hasError) {
+            return <div><p>ERROR!!</p></div>
+        }
         return (
             <div>
                 <h1>TITLE</h1>
